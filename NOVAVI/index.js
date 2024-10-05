45aa8b06d34f0bf7e31dc3839ae6d72cf7142cbd
Athilathesni <usmanchusman606@gmail.com>
2024-10-05 18:02:53 +0530

    file added

---
+++ NOVAVI/index.js
@@ -0,0 +1,30 @@
+document.addEventListener('mousemove', (event) => {
+    const circle = document.querySelector('.circle');
+
+    // Get cursor position
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+
+    // Move the circle to follow the mouse cursor
+    circle.style.left = `${mouseX - 25}px`;  // Adjust for half the width
+    circle.style.top = `${mouseY - 25}px`;   // Adjust for half the height
+});
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
